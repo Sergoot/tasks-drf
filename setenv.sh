4bab7a1f63a5b8fa781311b38d77088b26dd12d2
Sergoot <sergey.lyutavin22@mail.ru>
2022-07-12 10:36:42 +0300

    minor edits

---
+++ setenv.sh
@@ -0,0 +1,4 @@
+export SECRET_KEY='django-insecure-04keck!2z9o2o8oanb=%xl2-il82xfcl4vfa83+$*xz#_m-g*6'
+export EMAIL_HOST_USER='sergey.lyutavin22@gmail.com'
+export EMAIL_HOST_PASSWORD='aveqadrnelilgqiu'
+export ALLOWED_HOST="http://localhost:3000"
